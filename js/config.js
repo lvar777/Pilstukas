76ab702a5cf210312403167568c9f4e6d4b9adfe
Ezyyys <domcedomce35@gmail.com>
2026-04-19 19:28:27 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-export const MAP_DEFAULT_CENTER = [55.15, 24.9];
-export const MAP_DEFAULT_ZOOM = 8;
-
-export const FUEL_PRICES = {
-    diesel: 1.919,
-    petrol98: 1.746,
-    petrol95: 1.579,
-    lpg: 0.779
-};
